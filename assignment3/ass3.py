694a6441f019d0912a3bb252a708a6b64069ca05
Dan Kristensen <Dan@Dans-MBP.lan>
2017-01-10 14:51:45 +0100

    asss

--- assignment3/ass3.py
+++ assignment3/ass3.py
@@ -1,13 +1,41 @@
 import cv2
+import numpy as np
+#import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import os
+import itertools
 
-img = cv2.imread('lenna.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+des_array = np.array([])
 
-sift = cv2.xfeatures2d.SIFT_create()
-kp = sift.detect(gray, None)
-img2=cv2.drawKeypoints(gray,kp,img)
+def descriptors(image):
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	sift = cv2.xfeatures2d.SIFT_create()
+	kp, des = sift.detectAndCompute(gray,None)
+	return des
 
+def train(k,des_array):
+	km = KMeans(n_clusters = k)
+	km.fit(des_array)
+	return km
 
-cv2.imshow('image',img2)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# Retrieve files into training and test sets
+def splitFilesInDirectory(dir):
+	files = os.listdir(dir)
+	length = len(files)
+	training_files = [cv2.imread(dir + '/' + img) for img in files[length // 2:] if img[0] != "."]
+	test_files = [cv2.imread(dir + '/' + img) for img in files[:length // 2] if img[0] != "."]
+	return (training_files, test_files)
+
+# Training data
+(training_images, test_images) = splitFilesInDirectory('data')
+
+training_des_array = list(itertools.chain(*[descriptors(img) for img in training_images]))
+
+kkm = train(200,training_des_array)
+
+codebook = kkm.cluster_centers_
+
+training_labels = kkm.predict(descriptors(training_images[0]))
+test_labels = kkm.predict(descriptors(test_images[0]))
+
+print(test_labels)
