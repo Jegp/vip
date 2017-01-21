701bff8e7b015ecdad5f73b475e2619c48f73413
Dan Kristensen <Dan@Dans-MacBook-Pro.local>
2017-01-21 16:53:38 +0100

    123

--- assignment4/ass4.py
+++ assignment4/ass4.py
@@ -1,7 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-#from skimage.io import imshow
-import cv2
 import ps_utils as ps
 
 
@@ -17,18 +15,19 @@ m = np.dot(S,J)
 # p = Albedo, n = normal
 p = np.linalg.norm(m, axis=0)
 
-#n = (1/p)*m
-n = np.divide(m, p)
+n = (1/p)*m
+# = np.divide(m, p)
 
 
 n1 = np.zeros((256,256))
 n1[np.where(mask)] = n[0]
 n2 = np.zeros((256,256))
-n2[np.where(mask)] = n[0]
+n2[np.where(mask)] = n[1]
 n3 = np.zeros((256,256))
-n3[np.where(mask)] = n[0]
+n3[np.where(mask)] = n[2]
 
-
+#plt.imshow(n3)
+#plt.show()
 
 #depth_map = ps.simchony_integrate(n1,n2,n3,mask)
 depth_map = ps.unbiased_integrate(n1,n2,n3,mask, order=2)
